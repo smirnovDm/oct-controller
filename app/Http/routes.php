f5350681db8c8419a67e54ef9c92aef92744828e
user <user@user.com>
2019-05-14 20:53:24 +0300

    init news

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,15 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
+Route::get('/tasks', 'TaskController@index')->name('tasks_index');
+Route::post('/tasks', 'TaskController@store')->name('tasks_store');
+Route::delete('/tasks/{task}', 'TaskController@destroy')->name('tasks_destroy');
+//Route::resource('tasks', 'TaskController');
+//Route::resource('news', 'NewsController');
---
+++ app/Policies/TaskPolicy.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+use App\User;
+Use App\Task;
+
+class TaskPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Определяем, может ли данный пользователь удалить данную задачу.
+     *
+     * @param  User  $user
+     * @param  Task  $task
+     * @return bool
+     */
+    public function destroy(User $user, Task $task)
+    {
+        return $user->id === $task->user_id;
+    }
+}
